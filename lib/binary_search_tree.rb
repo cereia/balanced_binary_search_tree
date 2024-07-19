ddcf5605741ece765d6c7658b356eb6fc790c753
cereia <104402370+cereia@users.noreply.github.com>
2024-07-19 17:45:23 -0400

    Add initialize and build tree methods

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,1 +1,19 @@
 # frozen_string_literal: true
+
+# Tree class that holds all methods related to the creation and maintenance of a balanced binary search tree
+class Tree
+  def initialize(array)
+    @root = build_tree
+    @data = array.sort.uniq
+  end
+
+  def build_tree(arr = @data, start_index = 0, end_index = @data.length - 1)
+    return nil if start_index > end_index
+
+    mid = (start_index + end_index) / 2
+    node = Node.new(arr[mid])
+    node.left = build_tree(arr, start_index, mid - 1)
+    node.right = build_tree(arr, mid + 1, end_index)
+    node
+  end
+end
