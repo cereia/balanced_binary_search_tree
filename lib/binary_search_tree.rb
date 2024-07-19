e6fdbf74734caa944d1419758161dfa4317e01b6
cereia <104402370+cereia@users.noreply.github.com>
2024-07-19 19:28:49 -0400

    Add delete and min value methods
    Min value find the replacement value when the given value has 2 children

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -28,6 +28,34 @@ class Tree
     node
   end
 
+  def delete(value, node = @root)
+    return node if node.nil?
+
+    if value < node.value
+      node.left_child = delete(value, node.left_child)
+    elsif value > node.value
+      node.right_child = delete(value, node.right_child)
+    else
+      # when one or both children are nil, return the non-nil child if one exists
+      return node.right_child || node.left_child if node.left_child.nil? || node.right_child.nil?
+
+      # if both children are not nil, the node's value is set to the min value of the node's right descendants
+      node.value = min_value(node.right_child)
+      node.right_child = delete(node.value, node.right_child)
+    end
+    node
+  end
+
+  def min_value(node)
+    min_val = node.value
+    # while there is a left child, min value is set to that left child
+    while node.left_child
+      min_val = node.left_child.value
+      node = node.left_child
+    end
+    min_val
+  end
+
   def pretty_print(node = @root, prefix = '', is_left: true)
     pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", is_left: false) if node.right_child
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
--- main.rb
+++ main.rb
@@ -6,4 +6,9 @@ require_relative 'lib/binary_search_tree'
 tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 tree.insert(13)
 tree.insert(10)
+tree.delete(8)
 tree.pretty_print
+
+# test = Tree.new([20, 30, 40, 50, 60, 70, 80])
+# test.delete(50)
+# test.pretty_print
