d12603ecef24af3b3eecf9642ac43ba24bb1d022
cereia <104402370+cereia@users.noreply.github.com>
2024-07-19 17:22:54 -0400

    Add Node class that stores its value and children

--- lib/node.rb
+++ lib/node.rb
@@ -1,1 +1,10 @@
 # frozen_string_literal: true
+
+# Node class for a binary search tree that holds its own value and any children
+class Node
+  def initialize(data)
+    @value = data
+    @left_child = nil
+    @right_child = nil
+  end
+end
